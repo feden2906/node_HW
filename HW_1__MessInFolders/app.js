eee23f7908026ab63fa0d8ce1cbfb98e5a034ad6
Fedenko Maksym <feden2906@gmail.com>
2021-02-18 23:51:50 +0200

    last commit

--- HW_1__MessInFolders/app.js
+++ HW_1__MessInFolders/app.js
@@ -1,35 +1,33 @@
 const fs = require('fs');
 const path = require('path');
 
-
-function getAllFiles(rootPath, newFolder) {
-
-
+function getAllFiles(rootPath, newFolder, num) {
   fs.readdir(rootPath, (err, files) => {
     if (err) {
       return console.error(err)
     }
     files.forEach(file => {
       const newPath = path.join(rootPath, file);
-      const endPath = path.join(__dirname, newFolder, file)
+      const [name,ext] = file.split('.');
+      const endPath = path.join(__dirname, newFolder, `${name} копия ${num}.${ext}`)
+      let newIndex = num++;
       fs.stat(newPath, (err, stats) => {
         stats.isDirectory()
-            ? getAllFiles(newPath, newFolder)
+            ? getAllFiles(newPath, newFolder, newIndex)
             : moveFile(file, newPath, endPath)
       })
     })
   })
 }
 
-function moveFile(file, oldPath, newPath) {
+function moveFile(file, oldPath, endPath) {
   if (path.extname(file) === '.txt') {
-    console.log(newPath)
-    // fs.rename(oldPath, newPath, (err) => {
-    //   if (err) {
-    //     return console.log('file don`t move', err)
-    //   }
-    // })
+    fs.rename(oldPath, endPath, (err) => {
+      if (err) {
+        return console.log('file don`t move', err)
+      }
+    })
   }
 }
 
-getAllFiles(__dirname, 'newFolderForFiles')
+getAllFiles(__dirname, 'newFolderForFiles', 1)
