868be7bedcd329abb24993f254fbdd135187b3bd
Fedenko Maksym <feden2906@gmail.com>
2021-02-19 21:53:36 +0200

    pr

--- PR_1__Old-YangStudents/app.js
+++ PR_1__Old-YangStudents/app.js
@@ -6,3 +6,17 @@
 // вміст це сам ваш юзер - { name: 'olya', gender: 'female', age: 20 }
 // перед тим створити 4 папки - наприклад - manOlder20, manYounger20, womanOlder20, womanYounger20
 // і розподілити ваших юзерів саме по відповідних папках
+
+const users = [
+  {"id": 1, "age": 19, "name": "Alina", "gender": "female"},
+  {"id": 2, "age": 20, "name": "Anna", "gender": "female"},
+  {"id": 3, "age": 27, "name": "Ira", "gender": "female"},
+  {"id": 4, "age": 18, "name": "Oleksandr", "gender": "male"},
+  {"id": 5, "age": 15, "name": "Yaroslav", "gender": "male"},
+  {"id": 6, "age": 21, "name": "Boris", "gender": "male"},
+  {"id": 7, "age": 13, "name": "Dima", "gender": "male"},
+  {"id": 8, "age": 31, "name": "Oleg", "gender": "male"},
+  {"id": 9, "age": 25, "name": "Olga", "gender": "female"},
+  {"id": 10,"age": 17, "name": "Roman", "gender": "male"}
+]
+
