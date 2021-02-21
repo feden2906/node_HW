0326ca419768d6ff4790416c3577372202d48a58
Fedenko Maksym <feden2906@gmail.com>
2021-02-21 14:59:41 +0200

    fix register

--- HW_2__Register/app.js
+++ HW_2__Register/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const expressHbs = require('express-handlebars');
 const path = require('path');
-
+const fs = require('fs');
 
 const app = express();
 
@@ -16,20 +16,58 @@ app.set('view engine', '.hbs');
 app.engine('.hbs', expressHbs({defaultLayout: false}))
 app.set('views', path.join(__dirname, 'static'));
 
+const pathToDB = path.join(__dirname, 'users.json')
+
+
+function getUsers() {
+  return JSON.parse(fs.readFileSync(pathToDB));
+}
+function setUsers(users) {
+  fs.writeFile(pathToDB, JSON.stringify(users), (err => {
+    if (err) {
+      console.error(err)
+    }
+  }))
+}
 
 
 app.get('/login', (req, res) => {
   res.render('login');
 });
-app.post('/login', (req, res) => {
-  console.log(req.body)
+app.post('/login', ({body: {email, password}}, res) => {
+  const user = getUsers().some(user => user.email === email && user.password === password);
+  if (user) {
+    res.redirect('/users');
+    return;
+  }
+  res.redirect('/register');
 })
+
 
 app.get('/register', (req, res) => {
   res.render('register');
 })
+app.post('/register', ({body}, res) => {
+  const users = getUsers();
+  const userExists = users.some(user => user.email === body.email);
+  if (userExists) {
+    res.render('register', {error: true})
+    return
+  }
+  users.push(body);
+  setUsers(users);
+  res.redirect('/users');
+})
 
 
+app.get('/users', (req, res) => {
+  const users = getUsers();
+  res.render("users", {users});
+})
+
+app.post('/users', (req, res) => {
+
+})
 
 
 app.listen(5000, (err) => {
@@ -38,4 +76,6 @@ app.listen(5000, (err) => {
   }
   console.log('Server started')
 })
+
+
 
