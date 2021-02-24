7dbabbfd35ffe37447a8d06bbfce0b4352a110ff
Fedenko Maksym <feden2906@gmail.com>
2021-02-24 14:35:04 +0200

    update messages

--- HW_3__RoutesControllerMiddlewares/error/error.messages.js
+++ HW_3__RoutesControllerMiddlewares/error/error.messages.js
@@ -1,9 +1,4 @@
 module.exports = {
-  TOO_WEEK_PASSWORD: {
-    en: 'Too weak password',
-    ua: 'Твій пароль ацтой'
-  },
-
   USER_NOT_FOUND: {
     en: 'User not found',
     ua: 'Користувача не знайдено'
@@ -27,5 +22,20 @@ module.exports = {
   USER_WERE_DELETED: {
     en: 'User were deleted',
     ua: 'Користувача видалено'
+  },
+
+  TOO_WEEK_PASSWORD: {
+    en: 'Too weak password',
+    ua: 'Занадто короткий пароль'
+  },
+
+  NOT_VALID_EMAIL: {
+    en: 'Not Valid email',
+    ua: 'Некоректна електронна адреса'
+  },
+
+  NOT_VALID_ID: {
+    en: 'Not Valid ID',
+    ua: 'ID не валідний'
   }
 }
